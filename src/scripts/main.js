3a1e0c92a06f6ec706e6ce027d180645e5e2ae3d
Your Name <iyanperezmarcos@gmail.com>
2023-03-05 23:11:14 +0100

    preparation and timer section done [functionalitiy of preparation and start cooking botton missing]

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+const pizzaCbox = document.getElementById("cbox1");
+const ovenCbox = document.getElementById("cbox2");
+const respectCbox = document.getElementById("cbox3");
+
+const prepSection = document.getElementById("preparation-container");
+const timeSection = document.getElementById("time-container");
+
+
+function showPreparation(){
+    if (pizzaCbox == true && ovenCbox == true && respectCbox == true) {
+        
+    }
+}
