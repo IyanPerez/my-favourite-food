687c20c75cb038f6e91bdc42f5b569650a6da127
Your Name <iyanperezmarcos@gmail.com>
2023-03-19 19:27:43 +0100

    Check if ingredients checkbox are checked and show preparation/timer section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,43 @@
-const pizzaCbox = document.getElementById("cbox1");
-const pizzaCboxQuery = document.querySelectorAll(".cbox");
-const ovenCbox = document.getElementById("cbox2");
-const respectCbox = document.getElementById("cbox3");
-const startCookingBtn = document.getElementById("start-timer");
+// const pizzaCboxQuery = document.querySelectorAll(".cbox");
+let pizzaCbox1 = document.getElementById("cbox1");
+let pizzaCbox2 = document.getElementById("cbox2");
+let pizzaCbox3 = document.getElementById("cbox3");
 
+function check(){
+    if (pizzaCbox1.checked==true && pizzaCbox2.checked==true && pizzaCbox3.checked ==true) {
+        showPrepSection();
+    }
+}
+
+
+
+let prepSection = document.getElementById("preparation-time-container");
+let readyBtn = document.getElementById("ready-btn");
+
+readyBtn.addEventListener("click",() => {
+    check();
+    console.log(prepSection);
+}) 
+
+function showPrepSection(){
+    if (prepSection.style.display = "none") {
+        prepSection.style.display = "block"
+    }
+}
+// showPrepSection();
+
+
+
+
+
+
+
+let timeSection = document.getElementById("time-container");
+let startCookingBtn = document.getElementById("start-timer");
 
 startCookingBtn.addEventListener("click", () => {
     showTimer();
 })
-
-const prepSection = document.getElementById("preparation-container");
-const timeSection = document.getElementById("time-container");
-
-
 function showTimer(){
     if (timeSection.style.display = "none") {
         timeSection.style.display = "block"
@@ -20,5 +45,10 @@ function showTimer(){
 }
 
 
-
-
+// function checkIngredient() {
+//     pizzaCboxQuery.forEach(ingredient => {
+//         if (ingredient.checked == true) {
+//             console.log(ingredient.value);
+//         }
+//     });
+// }
